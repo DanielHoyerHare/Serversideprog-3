e0a2caffec1f208638754ea71679c2290c0c5a05
LoQhu <nick4453@elev.tec.dk>
2024-04-09 14:05:47 +0200

    Add project files.

---
+++ BlazorApp1/Models/ToDoContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Models;
+
+public partial class ToDoContext : DbContext
+{
+    public ToDoContext()
+    {
+    }
+
+    public ToDoContext(DbContextOptions<ToDoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cpr> Cprs { get; set; }
+
+    public virtual DbSet<ToDoList> ToDoLists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ToDo;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cpr>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Cpr__3214EC0797F7FBF4");
+
+            entity.ToTable("Cpr");
+
+            entity.Property(e => e.CprNr).HasMaxLength(500);
+            entity.Property(e => e.User).HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<ToDoList>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__ToDoList__3214EC07FE149D4A");
+
+            entity.ToTable("ToDoList");
+
+            entity.Property(e => e.Item).HasMaxLength(500);
+
+            entity.HasOne(d => d.User).WithMany(p => p.ToDoLists)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__ToDoList__UserId__2B3F6F97");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
